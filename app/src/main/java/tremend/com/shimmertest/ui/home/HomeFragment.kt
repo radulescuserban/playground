1ac7f3a36e83eebc314c45c262c5ed4f1c592595
Serban Radulescu <theodor.radu8@gmail.com>
2020-02-04 19:47:46 +0200

    Added rx1 and rx2 zip function tests.

--- app/src/main/java/tremend/com/shimmertest/ui/home/HomeFragment.kt
+++ app/src/main/java/tremend/com/shimmertest/ui/home/HomeFragment.kt
@@ -4,14 +4,26 @@ package tremend.com.shimmertest.ui.home
 import android.arch.lifecycle.ViewModelProviders
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import io.reactivex.functions.BiFunction
 import kotlinx.android.synthetic.main.fragment_home.*
+import rx.Observable
+import rx.android.schedulers.AndroidSchedulers
+import rx.schedulers.Schedulers
 import tremend.com.shimmertest.R
 
 
 class HomeFragment : Fragment() {
+
+    companion object {
+        private const val TAG = "HomeFragment"
+    }
 
     private lateinit var viewModel: HomeViewModel
 
@@ -30,9 +42,148 @@ class HomeFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         refreshFab.setOnClickListener { makeRxCalls() }
+
+        listenRx1textChanged()
+        testRx1ZipFunction()
+
+//        listenRx2textChanged()
+//        testRx2ZipFunction()
     }
 
     private fun makeRxCalls() {
         viewModel.getAllTags()
+    }
+
+    private fun listenRx1textChanged() {
+        inputRx1Et.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+                onRx1TextChanged(s.toString())
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+            }
+        })
+    }
+
+    private fun onRx1TextChanged(newRx1Input: String) {
+        getRx1Observable()
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe(
+                {
+                    toChangeTv.visibility = View.VISIBLE
+                    toChangeTv.text = newRx1Input + " has been added!"
+                },
+                {
+                    Log.e(TAG, "error in rx1", it)
+                })
+    }
+
+    private fun testRx1ZipFunction() {
+        startRx1ZipBtn.setOnClickListener {
+            Log.d(TAG, "in rx1 on click listener")
+
+            Observable.zip(getRx1Observable(), getSecondRx1Observable()) { _, _ ->
+                Log.d(TAG, "in rx1 zip")
+                toChangeTv.visibility = View.INVISIBLE
+                toChangeTv.visibility = View.VISIBLE
+                toChangeTv.text = "i was changed from a Rx1 zip function!"
+
+                true
+            }
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(
+                    {
+                        if (it) {
+                            Toast.makeText(context, "Rx1 onNext()", Toast.LENGTH_SHORT).show()
+                        }
+                    },
+                    {
+                        Log.e(TAG, "error in rx1 zip", it)
+                    })
+        }
+    }
+
+    private fun getRx1Observable(): Observable<String> {
+        return Observable.defer {
+            Observable.just("de ce nu mergi cu rx1")
+        }
+    }
+
+    private fun getSecondRx1Observable(): Observable<String> {
+        return Observable.defer {
+            Observable.just("test")
+        }
+    }
+
+    private fun listenRx2textChanged() {
+        inputRx2Et.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+                onRx2TextChanged(s.toString())
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+            }
+        })
+    }
+
+    private fun onRx2TextChanged(newRx2Input: String) {
+        val disposable = getRx2Observable()
+            .subscribeOn(io.reactivex.schedulers.Schedulers.io())
+            .observeOn(io.reactivex.android.schedulers.AndroidSchedulers.mainThread())
+            .subscribe(
+                {
+                    toChangeTv.visibility = View.VISIBLE
+                    toChangeTv.text = newRx2Input + " has been added!"
+                },
+                {
+                    Log.e(TAG, "error in rx2", it)
+                })
+    }
+
+    private fun testRx2ZipFunction() {
+        startRx1ZipBtn.setOnClickListener {
+            Log.d(TAG, "in rx2 on click listener")
+
+            io.reactivex.Observable.zip(getRx2Observable(), getSecondRx2Observable(),
+                BiFunction<String, String, Boolean> { _: String, _: String ->
+                    Log.d(TAG, "in rx2 zip")
+                    toChangeTv.visibility = View.VISIBLE
+                    toChangeTv.text = "i was changed from a Rx2 zip function!"
+
+                    true
+                }
+            )
+                .subscribeOn(io.reactivex.schedulers.Schedulers.io())
+                .observeOn(io.reactivex.android.schedulers.AndroidSchedulers.mainThread())
+                .subscribe(
+                    {
+                        if (it) {
+                            Toast.makeText(context, "Rx2 onNext()", Toast.LENGTH_SHORT).show()
+                        }
+                    },
+                    {
+                        Log.e(TAG, "error in rx2 zio", it)
+                    })
+        }
+    }
+
+    private fun getRx2Observable(): io.reactivex.Observable<String> {
+        return io.reactivex.Observable.defer {
+            io.reactivex.Observable.just("de ce mergi cu rx2")
+        }
+    }
+
+    private fun getSecondRx2Observable(): io.reactivex.Observable<String> {
+        return io.reactivex.Observable.defer {
+            io.reactivex.Observable.just("test 2")
+        }
     }
 }
